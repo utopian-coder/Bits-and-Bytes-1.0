14500530fdacd184dfc664054fa3e0a3516f392e
Imran Biswas <imranbiswas2209@gmail.com>
2022-10-03 19:56:09 +0530

    Fixed Landing Page!

--- src/components/Welcome/TagLine.js
+++ src/components/Welcome/TagLine.js
@@ -3,13 +3,12 @@ import classes from "./TagLine.module.css";
 const TagLine = () => {
   return (
     <div className={classes.tag}>
-      <p>BE CONSISTENT TO ACHIEVE A TARGET</p>
       <h1>Learn to Code effectively.</h1>
-      <p>
-        Boost your problem solving skills with consistent practice, and our
-        guidance.
-      </p>
-      <button>Sign Up</button>
+      <div className={classes["attract-lines"]}>
+        <p>Good resources build consistency.</p>
+        <p>Give us a chance and fall in love with coding.</p>
+      </div>
+      <button className={classes.btn}>Sign Up</button>
       <a href='#'>Get started </a>
     </div>
   );
